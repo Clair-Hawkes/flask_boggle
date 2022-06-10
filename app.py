f380894f69a584bf2b41fbb24b6d94fcc1398999
Clair-Hawkes <hawkesj7@gmail.com>
2022-06-10 15:35:23 -0700

    Score Word draft

--- app.py
+++ app.py
@@ -27,3 +27,37 @@ def new_game():
     games[game_id] = game
 
     return jsonify({"gameId": game_id, "board": game.board})
+
+@app.post('/api/score-word')
+def score_word():
+    """View Function score_word:
+    Is called via ajax in the browser.
+    The body of the request should include game_id, word
+    {'game_id':game_id,'word':'word'}
+
+    score_word will call below methods:
+    If method resolves false values:
+    1.) is_word_not_a_dup() => {result: "word-dup"}
+    2.) is_word_in_word_list() => {result: "not-word"}
+    3.) check_word_on_board() => {result: "not-on-board"}
+
+    If all methods resolve true: {result: "ok"}
+    """
+
+    game_id = request.json.get('game_id')
+    word = request.json.get('word')
+    game = games[game_id]
+
+    if not game.is_word_not_a_dup(word):
+        return jsonify({"result": "word-dup"})
+
+    if not game.is_word_in_word_list(word):
+        return jsonify({"result": "not-word"})
+
+    if not game.check_word_on_board(word):
+        return jsonify({"result": "not-on-board"})
+
+    return jsonify({"result": "ok"})
+
+
+    # await axios.post("/api/score-word", { /* your data goes here... */ } )
